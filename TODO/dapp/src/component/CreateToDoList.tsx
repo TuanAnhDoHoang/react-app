145743458e020a6ab1dbc41e23451a8136fce0a8
anhdoo <anhdoo1211@gmail.com>
2025-01-31 08:56:06 +0700

    comment in react

--- TODO/dapp/src/component/CreateToDoList.tsx
+++ TODO/dapp/src/component/CreateToDoList.tsx
@@ -15,18 +15,17 @@ export const CreateToDoList: React.FC<Props> = (props) => {
     const [digest, setDigest] = useState('');
     const { client, currentAccount, todoListObjectId } = props;
     const { mutate: signAndExecuteTransaction } = useSignAndExecuteTransaction({
-        execute: async ({ bytes, signature }) => {
-            return await client.executeTransactionBlock({
-                transactionBlock: bytes,
-                signature,
-                options: {
-                    showObjectChanges: true,
-                    showBalanceChanges: true,
-                }
-            });
-        }
+        // execute: async ({ bytes, signature }) => {
+        //     return await client.executeTransactionBlock({
+        //         transactionBlock: bytes,
+        //         signature,
+        //         options: {
+        //             showObjectChanges: true,
+        //             showBalanceChanges: true,
+        //         }
+        //     });
+        // }
     });
-    console.log('b');
     useEffect(() => {
         const createToDoList = async () => {
             const tx = new Transaction();
@@ -38,13 +37,13 @@ export const CreateToDoList: React.FC<Props> = (props) => {
                 {
                     transaction: tx,
                 },
-                {
-                    onSuccess: (result) => {
-                        console.log(result);
-                        setDigest(prev => prev = result.digest);
-                        alert(`create new list successfully ${result.digest}`);
-                    }
-                }
+                // {
+                //     onSuccess: (result) => {
+                //         console.log(result);
+                //         setDigest(prev => prev = result.digest);
+                //         alert(`create new list successfully ${result.digest}`);
+                //     }
+                // }
             );
             console.log(result);
         };
@@ -52,6 +51,6 @@ export const CreateToDoList: React.FC<Props> = (props) => {
         if(todoListObjectId.length > 0)localStorage.setItem("todoListObjectId", JSON.stringify(todoListObjectId));
     }, []) 
     return (
-        <div> hello </div>
+        <></>
     );
 }
