ca23f112285e455ab097babe24a6cdfe815c6b80
anhdoo <anhdoo1211@gmail.com>
2025-02-04 19:52:49 +0700

    header layout

--- tiktok/src/components/Layout/components/Header/index.js
+++ tiktok/src/components/Layout/components/Header/index.js
@@ -1,10 +1,29 @@
 import classNames from 'classnames/bind';
 import style from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-regular-svg-icons';
+import { faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 const cx = classNames.bind(style);
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok-logo" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search for videos and accounts" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('actions')}>Actions</div>
+            </div>
         </header>
     );
 }
